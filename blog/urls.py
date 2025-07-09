bf094cf798fd0047265e921a8db32f552f8a4438
zoenaarj5 <kacva@yahoo.com>
2025-07-09 08:34:50 +0200

    2025-07-09 - presentation text and picture added to homepage

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name="index"),
+    path("",views.news, name="indexNews"),
     path("news/",views.news, name="news"),
+    path("forum/",views.forum, name="forum"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request,"blog/home.html",{
-        "title":"Welcome to our blog"
-    })
+
 def news(request):
     return render(request,"blog/news.html",{
         "title":"Latest news"
+    })
+
+def forum(request):
+    return render(request,"blog/forum.html",{
+        "title":"La parole est à vous"
     })
